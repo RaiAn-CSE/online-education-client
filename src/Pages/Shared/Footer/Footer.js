b659635176d8a153459d95781bf44dfc334d1cde
RaiAn-CSE <robiul-1512392@diu.edu.bd>
2022-10-27 01:51:26 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Footer.css'
+import { FaFacebook, FaFacebookMessenger, FaTwitterSquare, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className='footer'>
-            <hr />
-            <h5>Programming Hero. © Online Education All rights reserved Copyrights 2022</h5>
-        </footer>
+        <footer>
+            <div class="footer-title">
+                <h2>Online Education</h2>
+            </div>
+            <hr></hr>
+            <div class="footer-menu">
+                <a href="#">Contact Us</a>
+                <a href="#">Terms of Service</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Privacy Settings</a>
+                <a href="#">Support</a>
+                <a href="#">Live Class</a>
+            </div>
+            <div class="social-media-link">
+                <a class="fb" href="https://web.facebook.com//?_rdc=1&_rdr"><FaFacebook></FaFacebook></a>
+                <a class="messenger" href="https://web.facebook.com//?_rdc=1&_rdr"><FaFacebookMessenger></FaFacebookMessenger></a>
+                <a class="twitter"
+                    href="https://twitter.com/?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%"><FaTwitterSquare></FaTwitterSquare></a>
+                <a class="youtube" href="https://www.youtube.com/"><FaYoutube></FaYoutube></a>
+            </div>
+            <h6 className='mt-3'>Copyright © 2022 Programming-hero.com</h6>
+        </footer >
     );
 };
 
